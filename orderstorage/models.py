0f3c0f6d16dec2baa9c255b8ff3b99f980d0bee1
coderoma <r.reset@gmail.com>
2016-09-11 23:15:57 +0700

    Added showcases, advertise and catalog models. Added STATICFILES_DIRS to settings. removed static folder from orderstorage app.

--- orderstorage/models.py
+++ orderstorage/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Advertising(models.Model):
+    pass
+
 class Showcase(models.Model):
     pass
+
+class Catalog(models.Model):
+    pass
+
